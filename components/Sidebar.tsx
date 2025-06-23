6295b54aa5ecc9ff19a6b0bffcd3424c25ab009a
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-06-23 17:23:52 -0500

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { Button } from './ui/button'
+import NewDocumentButton from './NewDocumentButton'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { MenuIcon } from 'lucide-react'
 
 const Sidebar = () => {
+
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+      {/* My Documents */}
+      {/* List... */}
+
+      {/* Shared Documents */}
+      {/* List... */}
+    </>
+  )
+
   return (
-    <div>
-      Sidebar
+    <div className='p-2 md:p-5 bg-gray-200 relative'>
+      <div className='md:hidden flex items-center justify-center'>
+
+      <Sheet>
+  <SheetTrigger>
+    <MenuIcon />
+  </SheetTrigger>
+  <SheetContent side='left'>
+    <SheetHeader>
+      <SheetTitle>Menu</SheetTitle>
+      <div>
+        {menuOptions}
+      </div>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+
+</div>
+<div className='hidden md:inline'>
+  {menuOptions}
+</div>
     </div>
   )
 }
